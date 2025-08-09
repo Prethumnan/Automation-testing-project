fdf613add622df6315a00eda2cb7c243ae2a9e9a
prethumnan <prethumnan99@gmail.com>
2025-08-09 18:23:46 +0530

    did some changes in pom.xml and docker file

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,46 @@
-# Use Maven image with JDK
-FROM maven:3.8.4-openjdk-17
+# Use Maven with JDK 17
+FROM maven:3.9.4-eclipse-temurin-17
 
-# Set working directory inside the container
+# Set working directory
 WORKDIR /app
 
-# Copy everything into the container
+# Copy project files
 COPY . .
 
-# Run Maven test
+# Install dependencies and Google Chrome
+RUN apt-get update && apt-get install -y \
+    wget \
+    unzip \
+    curl \
+    gnupg \
+    fonts-liberation \
+    libappindicator3-1 \
+    libasound2 \
+    libatk-bridge2.0-0 \
+    libatk1.0-0 \
+    libcups2 \
+    libdbus-1-3 \
+    libgdk-pixbuf2.0-0 \
+    libnspr4 \
+    libnss3 \
+    libx11-xcb1 \
+    libxcomposite1 \
+    libxdamage1 \
+    libxrandr2 \
+    xdg-utils \
+    && rm -rf /var/lib/apt/lists/*
+
+# Install Google Chrome (stable) with dependencies
+RUN curl -fsSL https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor -o /usr/share/keyrings/google-chrome.gpg \
+    && echo "deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list \
+    && apt-get update \
+    && apt-get install -y google-chrome-stable \
+    && rm -rf /var/lib/apt/lists/*
+
+
+# Set environment variables for headless mode
+ENV headless=true
+ENV browser=chrome
+
+# Run tests using Maven
 CMD ["mvn", "clean", "test"]
--- src/test/java/Base.java
+++ src/test/java/Base.java
@@ -1,6 +1,7 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -33,29 +34,48 @@ public class Base {
         }
     }
 
-    public WebDriver initializeWebDriver(){
+    public WebDriver initializeWebDriver() {
 
         String name = prop.getProperty("browser");
-        if (name.equalsIgnoreCase("chrome")){
+        boolean isHeadless = Boolean.parseBoolean(prop.getProperty("headless", "false"));
 
+        if (name.equalsIgnoreCase("chrome")) {
             WebDriverManager.chromedriver().setup();
-            driver= new ChromeDriver();
+            ChromeOptions options = new ChromeOptions();
+            if (isHeadless) {
+                options.addArguments("--headless=new");
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+                options.addArguments("--disable-gpu");
+                options.addArguments("--window-size=1920,1080");
+            }
+            driver = new ChromeDriver(options);
 
-        } else if (name.equalsIgnoreCase("firefox")) {
+        } /*else if (name.equalsIgnoreCase("firefox")) {
             WebDriverManager.firefoxdriver().setup();
-            driver = new FirefoxDriver();
+            FirefoxOptions options = new FirefoxOptions();
+            if (isHeadless) {
+                options.addArguments("--headless");
+                options.addArguments("--window-size=1920,1080");
+            }
+            driver = new FirefoxDriver(options);
 
         } else if (name.equalsIgnoreCase("edge")) {
             WebDriverManager.edgedriver().setup();
-            driver = new EdgeDriver();
-        }
+            EdgeOptions options = new EdgeOptions();
+            if (isHeadless) {
+                options.addArguments("headless");
+                options.addArguments("window-size=1920,1080");
+            }
+            driver = new EdgeDriver(options);
+        }*/
 
         driver.manage().window().maximize();
         driver.get("http://www.automationpractice.pl/index.php");
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 
-        return  driver;
+        return driver;
     }
 
     public String testdata(String s){
